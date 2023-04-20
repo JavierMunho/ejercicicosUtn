9fddd2d0b3424f9fe494d1df44e0d4d6f274ad4d
Javier Munhó <99562451+JavierMunho@users.noreply.github.com>
2023-04-20 00:04:02 -0300

    nuevo
    primer ejercicio

---
+++ pruebas/src/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Vector y= new vector();
+
+        y.add(0, "Ingreso posicion 0");
+        y.add(1, "Ingreso posicion 1");
+        y.add(2, "Ingreso posicion 2");
+        y.add("Ingresando despues del ultimo ingreso");
+        y.add(4, "Ingreso posicion 4");
+
+    }
+    for()
+        System.out.println( i +" "+ y.get(i));
+
+}
